58fbbd6797e56ce6ed8c2349ca3f29448e7e6958
tramitong90 <tramitong90@gmail.com>
2024-01-22 09:20:32 -0600

    Mi listOfProduct

--- costcoproject/src/test/java/test/com/ListOfProductInMeatSeafoodTest.java
+++ costcoproject/src/test/java/test/com/ListOfProductInMeatSeafoodTest.java
@@ -64,7 +64,7 @@ public class ListOfProductInMeatSeafoodTest {
         
         driver.findElement(By.xpath("//div[@id='productTile_4000149679']/div[2]/div[2]/div/div/div/div/div/button/span")).click();
         driver.get("https://signin.costco.com/e0714dd4-784d-46d6-a278-3e29553483eb/B2C_1A_SSO_WCS_signup_signin_567/oauth2/v2.0/authorize?ClientName=USBC&ui_locales=en-US&scope=openid+offline_access&response_type=code+id_token&redirect_uri=https%3A%2F%2Fwww.costco.com%2FOAuthLogonCmd&state=URL%3D%5BSSO%5Dhttps%3A%2F%2Fwww.costco.com%2Fmeat.html%5BSSO%5D&nonce=wlBg7V6jWHTTqAIO&client_id=4900eb1f-0c10-4bd9-99c3-c59e6c1ecebf&TC=0&response_mode=form_post");
-        assertEquals(driver.getTitle(), "Sign In");
+        assertEquals(driver.getTitle(), "Sign In | Costco");
     }
     
     @Test
