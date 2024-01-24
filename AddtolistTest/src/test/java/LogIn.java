710afac01371ac0cffbb18fe1a7b8eebbc7d3d6d
Zoya-K <zoyasalnikova@yahoo.com>
2024-01-24 14:32:04 -0600

    Add to list and Sign in test cases are added

---
+++ AddtolistTest/src/test/java/LogIn.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author navat
+ */
+public class LogIn {
+    private String username;
+    private String password;
+
+    public LogIn(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LogIn{" + "username=" + username + ", password=" + password + '}';
+    }
+    
+    
+}
